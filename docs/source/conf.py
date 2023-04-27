20b77d60d2b923a1bf8e5ff2b62dace3429ce707
Kaikai Liu <lkk688@gmail.com>
2023-04-27 11:31:02 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MyTutorial'
+copyright = '2023, Kaikai Liu'
+author = 'Kaikai Liu'
 
 release = '0.1'
 version = '0.1.0'
